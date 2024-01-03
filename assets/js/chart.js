0449ecdee1d71793b796f0293c7326932ba8d69b
Narayana Rao Bhogapurapu <narayanarao.bhogapurapu@gmail.com>
2024-01-03 13:28:04 -0500

    minor update

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -39,7 +39,7 @@ var myChart = new Chart(ctx, {
         labels: [" ", "2020", "2021", "2022","2023"],
         datasets: [{
             label: '# citations',
-            data: [0,4, 30, 67, 77],
+            data: [0,4, 30, 67, 84],
             backgroundColor: [
                 // 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
